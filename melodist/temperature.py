8f7b468f6965e86b21ccb726badcc51522db2ce0
Florian Hanzer <florian.hanzer@uibk.ac.at>
2016-08-12 10:09:51 +0200

    Vectorize temperature disaggregation

--- melodist/temperature.py
+++ melodist/temperature.py
@@ -27,224 +27,157 @@ from __future__ import print_function, division, absolute_import
 import melodist
 import melodist.util
 import numpy as np
-
-"""
-This routine diaggregates daily values of temperature data to hourly values
-"""
-
-import re
 import pandas as pd
-from math import sin, cos, pi
 
-
-def disaggregate_temperature(dailyData, method='sine', min_max_time='fix', mod_nighttime = False, max_delta=None, sun_times=None):
+def disaggregate_temperature(data_daily, method='sine', min_max_time='fix', mod_nighttime=False, max_delta=None, sun_times=None):
     """The disaggregation function for temperature
 
     Parameters
     ----
-    dailyData :       daily data
-    method :          method to dissaggregate
-    min_max_time:     "fix" - min/max temperature at fix times 7h/14h,
+    data_daily :      daily data
+    method :          method to disaggregate
+    min_max_time:     "fix" - min/max temperature at fixed times 7h/14h,
                       "sun_loc" - min/max calculated by sunrise/sunnoon + 2h,
-                      "sun_loc_shift" - min/max calculated by sunrise/sunnoon + monthly mean shift ,                     
+                      "sun_loc_shift" - min/max calculated by sunrise/sunnoon + monthly mean shift,
     max_delta:        maximum monthly temperature shift as returned by get_shift_by_data()
     sun_times:        times of sunrise/noon as returned by get_sun_times()
     """
-
-    # get length of time series in days
-    days = len(dailyData.index)
-
-    # perform disaggregation depending on chosen option
 
     if method not in (
         'sine',
     ):
         raise ValueError('Invalid option')
 
-    temp_disagg = pd.Series(index=melodist.util.hourly_index(dailyData.index))
+    temp_disagg = pd.Series(index=melodist.util.hourly_index(data_daily.index))
 
-###############################################################################
-
-    # for this option assume maximum occur at 14h and minimm at 7h and fit cosine function through minimum and maximum temperatures
     if method == 'sine':
-        # set some parameters for cosine function
-        steps = 24
+        # for this option assume time of minimum and maximum and fit cosine function through minimum and maximum temperatures
+        hours_per_day = 24
+        default_shift_hours = 2
 
-        # init first value for polar night disaggregation
-        last_val = 0.5 * (dailyData.tmin[[0]] + dailyData.tmin[[0]])
-        model_transition = False
         daylength_thres = 3
         # min / max hour during polar night assumption
-        minloc = 6
-        maxloc = 18
- 
+        min_loc_polar = 6
+        max_loc_polar = 18
 
-        # loop over all days in time series
-        counter = 0
-        for index, row in dailyData.iterrows():
-            
-            #take fix location for minimum and maximum
-            if min_max_time== "fix":
-                minLocation= 7
-                maxLocation= 14
-        
-            #take location for minimum and maximum by sunrise / sunnoon + 2h
-            elif min_max_time== "sun_loc":
-                minLocation= int(round(sun_times.sunrise[index]))      #sun rise round to full hour 
-                maxLocation= int(round(sun_times.sunnoon[index])) + 2  #sun noon round to full hour + fix 2h
-            
-            #take location for minimum and maximum by sunrise / sunnoon + monthly delta
-            elif min_max_time== "sun_loc_shift":
-                minLocation= int(round(sun_times.sunrise[index]))                  #sun rise round to full hour 
-                maxLocation= int(round(sun_times.sunnoon[index] + max_delta[index.month]))  #sun noon + shift derived from obsereved h data, round to full hour
-                if minLocation > maxLocation:
-                    maxLocation= int(round(sun_times.sunnoon[index])) + 2 # standard shift in this case
-                # month -1 ???
-                
-            # define minimum value for cosine function to assure smooth transitions
-            # for last timestep there is no "next step" so consider two cases in minValue calculation
-            if counter == days - 1 :   
-                minValueAct = row.tmin
-                minValueNext = row.tmin
-                maxValueNext = row.tmax
-                
-            else:
-                minValueAct = row.tmin
-                minValueNext = dailyData.tmin[index.date() + pd.Timedelta('1 days')] #get min value of next day
-                maxValueNext = dailyData.tmax[index.date() + pd.Timedelta('1 days')]
+        locdf = pd.DataFrame(
+            index=data_daily.index,
+            columns=[
+                'min_loc',
+                'max_loc',
+                'min_val_before',
+                'min_val_cur',
+                'min_val_next',
+                'max_val_before',
+                'max_val_cur',
+                'max_val_next',
+            ]
+        )
 
+        if min_max_time == 'fix':
+            # take fixed location for minimum and maximum
+            locdf.min_loc = 7
+            locdf.max_loc = 14
+        elif min_max_time == 'sun_loc':
+            # take location for minimum and maximum by sunrise / sunnoon + 2h
+            locdf.min_loc = sun_times.sunrise.round() # sun rise round to full hour
+            locdf.max_loc = sun_times.sunnoon.round() + default_shift_hours # sun noon round to full hour + fix 2h
+        elif min_max_time == 'sun_loc_shift':
+            # take location for minimum and maximum by sunrise / sunnoon + monthly delta
+            locdf.min_loc = sun_times.sunrise.round() # sun rise round to full hour
+            locdf.max_loc = (sun_times.sunnoon + max_delta[locdf.index.month].values).round() # sun noon + shift derived from observed hourly data, round to full hour
 
-            # define maximum value for cosine function to assure smooth transitions
-            # for first timestep there is no "step before" so consider two cases in maxValue calculation
-            if counter == 0 :
-                maxValueAct = row.tmax
-                maxValueBefore = row.tmax
-                minValueBefore = row.tmin
+            pos = locdf.min_loc > locdf.max_loc
+            locdf.loc[pos, 'max_loc'] = sun_times.sunnoon[pos].round() + default_shift_hours # standard shift in this case
 
-            else:
-                maxValueAct = row.tmax
-                maxValueBefore = dailyData.tmax[index.date() - pd.Timedelta('1 days')] #get max value of day before
-                minValueBefore = dailyData.tmin[index.date() - pd.Timedelta('1 days')] #get max value of day before
-            
-            # get value for current hour
-            dayIndex = str(index)
-            for hour in range(0, 24):
-                if hour < 10:
-                    hourIndex = dayIndex[0:11]+'0'+str(hour)+dayIndex[13:]
-                else:
-                    hourIndex = dayIndex[0:11]+str(hour)+dayIndex[13:]
-    
-                # during polar night, no diurnal variation of temperature is applied
-                # instead the daily average calculated using tmin and tmax is applied
-                if sun_times.daylength[index] < daylength_thres:
-                    # temp_disagg[hourIndex] = 0.5 * (row.tmin + row.tmax)
-                    avgBefore  = 0.5 * (minValueBefore + maxValueBefore)
-                    avgCurrent = 0.5 * (minValueAct + maxValueAct)
-                    avgNext    = 0.5 * (minValueNext + maxValueNext)
-                    
-                    maxloc_transition = maxloc                    
-                    
-                    if model_transition and hour <= minloc:
-                        last_val = maxValueBefore
-                        maxloc_transition = maxLocation
-                        if hour == minloc:
-                            model_transition = False
-                                                                                        
-                    if avgBefore <= avgCurrent:
-                        val1 = minValueAct
-                        val2 = maxValueAct
-                    else:
-                        val1 = maxValueAct
-                        val2 = minValueAct
+        locdf.min_loc = locdf.min_loc.astype(int)
+        locdf.max_loc = locdf.max_loc.astype(int)
 
-                    if hour <= minloc:
-                        funcValue = last_val + (hour + 24 - maxloc_transition)/ (24 - maxloc_transition + minloc) * (val1 - last_val)
-                    elif hour > minloc and hour <= maxloc:
-                        funcValue = val1 + (hour - minloc)/ (maxloc - minloc) * (val2 - val1)
-                    else:
-                        if avgCurrent <= avgNext:
-                            next_val = minValueNext
-                        else:
-                            next_val = maxValueNext
+        locdf.min_val_cur = data_daily.tmin
+        locdf.max_val_cur = data_daily.tmax
+        locdf.min_val_next = data_daily.tmin.shift(-1, 'D')
+        locdf.max_val_next = data_daily.tmax.shift(-1, 'D')
+        locdf.loc[locdf.index[-1], 'min_val_next'] = locdf.min_val_cur.iloc[-1]
+        locdf.loc[locdf.index[-1], 'max_val_next'] = locdf.max_val_cur.iloc[-1]
+        locdf.min_val_before = data_daily.tmin.shift(1, 'D')
+        locdf.max_val_before = data_daily.tmax.shift(1, 'D')
+        locdf.loc[locdf.index[0], 'min_val_before'] = locdf.min_val_cur.iloc[0]
+        locdf.loc[locdf.index[0], 'max_val_before'] = locdf.max_val_cur.iloc[0]
 
-                        next_time = minloc
-                        # check whether to use cosine function in next time step
-                        if counter < days - 1:
-                            if sun_times.daylength[counter+1] >= daylength_thres:
-                                next_val = minValueNext
-                                next_time = minLocation
-                                if hour == 23:
-                                    model_transition = True
+        locdf_day = locdf
+        locdf = locdf.reindex(temp_disagg.index, method='ffill')
 
-                        funcValue = val2 + (hour - maxloc)/ (24 - maxloc + next_time) * (next_val - val2)
-                        last_val = val2
-                        
+        # whenever we are before the maximum for the current day, use minimum value of current day for cosine function fitting
+        # once we have passed the maximum value use the minimum for next day to ensure smooth transitions
+        min_val = locdf.min_val_next.copy()
+        min_val[min_val.index.hour < locdf.max_loc] = locdf.min_val_cur
 
-                else:
-                    # whenever we are before the maximum for the current day, use minimum value of current day for cosine function fitting
-                    # once we have passed the maximum value use the minimum for next day to ensure smooth transitions
-                    if hour < maxLocation:
-                        minValue = minValueAct
-                    else:
-                        minValue = minValueNext
-    
-                    # whenever we are before the minimum for the current day, use maximum value of day before for cosine function fitting
-                    # once we have passed the minimum value use the maximum for the actual day to ensure smooth transitions
-                    if hour < minLocation:
-                        maxValue = maxValueBefore
-                    else:
-                        maxValue = maxValueAct               
-                    
-                    deltaValue = maxValue - minValue
-                    vTrans = minValue + (0.5*deltaValue)
-    
-                    if mod_nighttime:
-                        if hour <= minLocation:
-                            funcValue = vTrans + (deltaValue*0.5) * cos(pi/(steps - (maxLocation-minLocation))*(steps - maxLocation + hour))
-                        elif hour > minLocation and hour < maxLocation:
-                            funcValue = vTrans + (deltaValue*0.5) * cos(1.25*pi + 0.75*pi/(maxLocation-minLocation)*(hour-minLocation))
-                        else:
-                            funcValue = vTrans + (deltaValue*0.5) * cos(pi/(steps - (maxLocation-minLocation))*(hour-maxLocation))
-                    else:
-                        funcValue = vTrans + (deltaValue*0.5) * cos(2*pi/steps*(hour-maxLocation))
+        # whenever we are before the minimum for the current day, use maximum value of day before for cosine function fitting
+        # once we have passed the minimum value use the maximum for the current day to ensure smooth transitions
+        max_val = locdf.max_val_cur.copy()
+        max_val[max_val.index.hour < locdf.min_loc] = locdf.max_val_before
 
-                    # overwrite values for model transition
-                    if model_transition and hour < minLocation:
-                        funcValue = last_val + (hour + 24 - maxloc)/ (24 - maxloc + minLocation) * (minValueAct - last_val)
+        delta_val = max_val - min_val
+        v_trans = min_val + delta_val / 2.
+        temp_disagg = pd.Series(index=min_val.index)
 
-                    if model_transition and hour == minLocation:
-                        model_transition = False
-                    
-                    # check whether to use cosine function in next time step
-                    if counter < days - 1:
-                        if sun_times.daylength[counter+1] < daylength_thres:
-                            if hour == maxLocation:
-                                avgCurrent = 0.5 * (minValueAct + maxValueAct)
-                                avgNext    = 0.5 * (minValueNext + maxValueNext)
-                                if avgCurrent < avgNext:
-                                    val = minValueNext
-                                else:
-                                    val = maxValueNext
-                            if hour > maxLocation:
-                                funcValue = maxValueAct + (hour - maxLocation)/ (24 - maxLocation + minloc) * (val - maxValueAct)
-                                if hour == 23:
-                                    model_transition = True
-                                    last_val = maxValueAct
-                        
-                    # correction when tmax current day < tmin next day
-                    #if hour >= maxLocation: #if maxValueAct < minValueNext and hour >= maxLocation:
-                    #    funcValue = maxValueAct + (hour - maxLocation) / (24 - maxLocation + minLocation) * (minValueNext - maxValueAct)
+        if mod_nighttime:
+            before_min = locdf.index.hour <= locdf.min_loc
+            between_min_max = (locdf.index.hour > locdf.min_loc) & (locdf.index.hour < locdf.max_loc)
+            after_max = locdf.index.hour >= locdf.max_loc
+            temp_disagg[before_min]      = v_trans + delta_val / 2. * np.cos(np.pi / (hours_per_day - (locdf.max_loc - locdf.min_loc)) * (hours_per_day - locdf.max_loc + locdf.index.hour))
+            temp_disagg[between_min_max] = v_trans + delta_val / 2. * np.cos(1.25 * np.pi + 0.75 * np.pi / (locdf.max_loc - locdf.min_loc) * (locdf.index.hour - locdf.min_loc))
+            temp_disagg[after_max]       = v_trans + delta_val / 2. * np.cos(np.pi / (hours_per_day - (locdf.max_loc - locdf.min_loc)) * (locdf.index.hour - locdf.max_loc))
+        else:
+            temp_disagg = v_trans + (delta_val / 2.) * np.cos(2 * np.pi / hours_per_day * (locdf.index.hour - locdf.max_loc))
 
-                    #if hour < minLocation: #if maxValueBefore < minValueAct and hour <= minLocation:
-                    #    funcValue = maxValueBefore + (hour + 24 - maxLocation)/ (24 - maxLocation + minLocation) * (minValueAct - maxValueBefore)
-                     
-                temp_disagg[hourIndex] = funcValue
+        polars = sun_times.daylength < daylength_thres
+        if polars.sum() > 0:
+            # during polar night, no diurnal variation of temperature is applied
+            # instead the daily average calculated using tmin and tmax is applied
+            polars_index_hourly = melodist.util.hourly_index(polars[polars].index)
+            temp_disagg.loc[polars_index_hourly] = np.nan
 
-            counter = counter + 1
+            avg_before = (locdf_day.min_val_before + locdf_day.max_val_before) / 2.
+            avg_cur = (locdf_day.min_val_cur + locdf_day.max_val_cur) / 2.
+            getting_warmers = polars &  (avg_before <= avg_cur)
+            getting_colders = polars & ~(avg_before <= avg_cur)
+
+            getting_warmers_min_loc = pd.DatetimeIndex([ts.replace(hour=min_loc_polar) for ts in getting_warmers[getting_warmers].index])
+            getting_warmers_max_loc = pd.DatetimeIndex([ts.replace(hour=max_loc_polar) for ts in getting_warmers[getting_warmers].index])
+            temp_disagg[getting_warmers_min_loc] = locdf_day.min_val_cur[getting_warmers].values
+            temp_disagg[getting_warmers_max_loc] = locdf_day.max_val_cur[getting_warmers].values
+
+            getting_colders_min_loc = pd.DatetimeIndex([ts.replace(hour=min_loc_polar) for ts in getting_colders[getting_colders].index])
+            getting_colders_max_loc = pd.DatetimeIndex([ts.replace(hour=max_loc_polar) for ts in getting_colders[getting_colders].index])
+            temp_disagg[getting_colders_min_loc] = locdf_day.max_val_cur[getting_colders].values
+            temp_disagg[getting_colders_max_loc] = locdf_day.min_val_cur[getting_colders].values
+
+            temp_polars = temp_disagg.loc[polars_index_hourly].copy()
+            transition_days = polars[polars.diff() == True].astype(int) # 0 where transition from polar to "normal" mode, 1 where transition from normal to polar
+
+            if len(transition_days) > 0:
+                polar_to_normal_days = transition_days.index[transition_days == 0]
+                normal_to_polar_days = transition_days.index[transition_days == 1] - pd.Timedelta(days=1)
+                add_days = polar_to_normal_days.union(normal_to_polar_days)
+
+                temp_polars = temp_polars.append(temp_disagg[melodist.util.hourly_index(add_days)]).sort_index()
+
+                for day in polar_to_normal_days:
+                    min_loc = int(locdf.loc[day].min_loc)
+                    temp_polars[day.replace(hour=0):day.replace(hour=min_loc) - pd.Timedelta(hours=1)] = np.nan
+                    temp_polars[day.replace(hour=min_loc)] = locdf.min_val_cur[day]
+
+                for day in normal_to_polar_days:
+                    max_loc = int(locdf.loc[day].max_loc)
+                    temp_polars[day.replace(hour=max_loc) + pd.Timedelta(hours=1):day.replace(hour=23)] = np.nan
+
+            temp_interp = temp_polars.interpolate(method='linear', limit=23)
+            temp_disagg[temp_interp.index] = temp_interp
 
     return temp_disagg
 
-###############################################################################
+
 def get_shift_by_data(temp_hourly, lon, lat, time_zone):
     '''function to get max temp shift (monthly) by hourly data
     
