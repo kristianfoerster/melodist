cca967bac972bf6ab2d79477d23040e9808135f1
Florian Hanzer <florian.hanzer@uibk.ac.at>
2017-11-02 11:46:01 +0100

    Implement __deepcopy__ for Bunch class

--- melodist/util/bunch.py
+++ melodist/util/bunch.py
@@ -23,7 +23,10 @@
 #                                                                                                             #
 ###############################################################################################################
 
-# source: http://code.activestate.com/recipes/52308-the-simple-but-handy-collector-of-a-bunch-of-named
+import copy
+
+
+# based on http://code.activestate.com/recipes/52308-the-simple-but-handy-collector-of-a-bunch-of-named
 class Bunch(dict):
     def __init__(self, **kw):
         dict.__init__(self, kw)
@@ -32,6 +35,9 @@ class Bunch(dict):
     def __copy__(self):
         copy = type(self)(**self)
         return copy
+
+    def __deepcopy__(self, memo):
+        return Bunch(**copy.deepcopy(dict(self)))
 
     def copy(self):
         return self.__copy__()
