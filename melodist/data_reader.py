1577a90e71f90d2afacff0bd3cf62e3bbff57174
Florian Hanzer <florian.hanzer@uibk.ac.at>
2017-12-21 09:20:31 +0100

    Move KNMI import functions from read_knmi_data.py to data_reader.py, delete read_csv function

--- melodist/data_reader.py
+++ melodist/data_reader.py
@@ -23,17 +23,16 @@
 #                                                                                                             #
 ###############################################################################################################
 
+import glob
 import melodist
 import pandas as pd
 import numpy as np
 import collections
 
-###############################################################################
-#reads smet data
 
 def read_smet(filename, mode):
     """Reads smet data and returns the data in required dataformat (pd df)
-    
+
     See https://models.slf.ch/docserver/meteoio/SMET_specifications.pdf
     for further details on the specifications of this file format.
 
@@ -50,24 +49,22 @@ def read_smet(filename, mode):
     """
 
     # dictionary
-    #based on smet spec V.1.1 and self defined
-    #daily data
-    dict_d=   {'TA':'tmean',
-               'TMAX':'tmax',   #no spec
-               'TMIN':'tmin',   #no spec
-               'PSUM':'precip',
-               'ISWR':'glob',     #no spec
-               'RH':'hum',
-               'VW':'wind'
-                }
+    # based on smet spec V.1.1 and self defined
+    # daily data
+    dict_d = {'TA': 'tmean',
+              'TMAX': 'tmax',   # no spec
+              'TMIN': 'tmin',   # no spec
+              'PSUM': 'precip',
+              'ISWR': 'glob',     # no spec
+              'RH': 'hum',
+              'VW': 'wind'}
 
-    #hourly data
-    dict_h=   {'TA':'temp',
-               'PSUM':'precip',
-               'ISWR':'glob',     #no spec
-               'RH':'hum',
-               'VW':'wind'
-                }
+    # hourly data
+    dict_h = {'TA': 'temp',
+              'PSUM': 'precip',
+              'ISWR': 'glob',     # no spec
+              'RH': 'hum',
+              'VW': 'wind'}
 
     with open(filename) as f:
         in_header = False
@@ -89,11 +86,9 @@ def read_smet(filename, mode):
                 v = line_split[1].strip()
                 header[k] = v
 
-
-    #get column names
+    # get column names
     columns = header['fields'].split()
     multiplier = [float(x) for x in header['units_multiplier'].split()][1:]
-    
 
     data = pd.read_table(
         filename,
@@ -109,7 +104,7 @@ def read_smet(filename, mode):
 
     del data.index.name
 
-    #rename columns
+    # rename columns
     if mode == "d":
         data = data.rename(columns=dict_d)
     if mode == "h":
@@ -117,8 +112,6 @@ def read_smet(filename, mode):
 
     return header, data
     
-################################################################################
-#read dwd data
 
 def read_dwd(filename, metadata, mode="d", skip_last=True):
     """Reads dwd (German Weather Service) data and returns the data in required
@@ -137,86 +130,81 @@ def read_dwd(filename, metadata, mode="d", skip_last=True):
     header:    header as dict
     data :     data as pd df
     """
-    
-    #--------------------------------------------------------------------------
-    def read_single_dwd(filename, metadata, mode, skip_last):
 
-        #Param names {'DWD':'dissag_def'}
-        dict_d=   {'LUFTTEMPERATUR':'tmean',
-                   'LUFTTEMPERATUR_MINIMUM':'tmin',   #no spec
-                   'LUFTTEMPERATUR_MAXIMUM':'tmax',   #no spec
-                   'NIEDERSCHLAGSHOEHE':'precip',
-                   'GLOBAL_KW_J':'glob',     #no spec
-                   'REL_FEUCHTE':'hum',
-                   'WINDGESCHWINDIGKEIT':'wind',
-                   'SONNENSCHEINDAUER':'sun_h'
-                    }
-        
-        #---read meta------------------
+    def read_single_dwd(filename, metadata, mode, skip_last):
+        # Param names {'DWD':'dissag_def'}
+        dict_d = {'LUFTTEMPERATUR': 'tmean',
+                  'LUFTTEMPERATUR_MINIMUM': 'tmin',   # no spec
+                  'LUFTTEMPERATUR_MAXIMUM': 'tmax',   # no spec
+                  'NIEDERSCHLAGSHOEHE': 'precip',
+                  'GLOBAL_KW_J': 'glob',     # no spec
+                  'REL_FEUCHTE': 'hum',
+                  'WINDGESCHWINDIGKEIT': 'wind',
+                  'SONNENSCHEINDAUER': 'sun_h'}
+
+        # ---read meta------------------
         meta = pd.read_csv(
             metadata,
             sep=';'
             )
-        
-        #remove whitespace from header columns
+
+        # remove whitespace from header columns
         meta.rename(columns=lambda x: x.strip(), inplace=True)
-        
-        header = {"Stations_id" : meta.Stations_id[meta.last_valid_index()],
+
+        header = {"Stations_id": meta.Stations_id[meta.last_valid_index()],
                   "Stationsname": meta.Stationsname[meta.last_valid_index()],
-                  #workaround for colnames with . (Geogr.Breite)
-                  "Breite" : meta.iloc[meta.last_valid_index(),2], #DezDeg
-                  "Laenge" : meta.iloc[meta.last_valid_index(),3]  #DezDeg
+                  # workaround for colnames with . (Geogr.Breite)
+                  "Breite": meta.iloc[meta.last_valid_index(), 2],  # DezDeg
+                  "Laenge": meta.iloc[meta.last_valid_index(), 3]   # DezDeg
                   }
-        
-        #---read data------------------
-        if skip_last!=None:
+
+        # ---read data------------------
+        if skip_last is not None:
             num_lines = sum(1 for line in open(filename))
             skip_last = [num_lines-1]
-        
-        #hourly data must be parsed by custom definition
+
+        # hourly data must be parsed by custom definition
         if mode == "d":
             data = pd.read_csv(
                 filename,
                 sep=';',
-                na_values= '-999',
+                na_values='-999',
                 index_col=' MESS_DATUM',
                 parse_dates=True,
                 skiprows=skip_last
                 )
-        
-        #hourly data must be parsed by custom definition
+
+        # hourly data must be parsed by custom definition
         if mode == "h":
-    
             def date_parser(date_time):
                 hour = date_time[8:10]
                 day = date_time[6:8]
-                month= date_time[4:6]
+                month = date_time[4:6]
                 year = date_time[0:4]
                 minute = '00'
                 sec = '00'
-                return  pd.Timestamp('%s-%s-%s %s:%s:%s' % (year, month, day, hour, minute, sec))     
-            
+                return pd.Timestamp('%s-%s-%s %s:%s:%s' % (year, month, day, hour, minute, sec))
+
             data = pd.read_csv(
                 filename,
                 sep=';',
-                na_values= '-999',
+                na_values='-999',
                 index_col=' MESS_DATUM',
                 date_parser=date_parser,
                 skiprows=skip_last
                 )
-        
-        #remove whitespace from header columns
+
+        # remove whitespace from header columns
         data.rename(columns=lambda x: x.strip(), inplace=True)
-        
-        #rename to dissag definition
+
+        # rename to dissag definition
         data = data.rename(columns=dict_d)
-        #get colums which are not defined 
+        # get colums which are not defined
         drop = [col for col in data.columns if col not in dict_d.values()]
-        #delete columns
+        # delete columns
         data = data.drop(drop, axis=1)
-        
-        
-#        #convert temperatures to Kelvin (+273.15)
+
+        # convert temperatures to Kelvin (+273.15)
         if 'tmin' in data.columns:
             data["tmin"] = data["tmin"] + 273.15
         if 'tmax' in data.columns:
@@ -225,106 +213,42 @@ def read_dwd(filename, metadata, mode="d", skip_last=True):
             data["tmean"] = data["tmean"] + 273.15
         if 'temp' in data.columns:
             data["temp"] = data["temp"] + 273.15
-        
+
         return header, data
-    
-    #-------------------------------------------------------------------------- 
-    
-    if type(filename)==list:
+
+    if type(filename) == list:
         i = 1
         for file in filename:
             header, data_h = read_single_dwd(file, metadata, mode, skip_last)
-            
+
             if i == 1:
-                data = data_h           
-            else:          
+                data = data_h
+            else:
                 data = data.join(data_h, how='outer')
-            i+=1
-                
+            i += 1
+
     else:
         header, data = read_single_dwd(filename, metadata, mode, skip_last)
-        
+
     return header, data
 
-################################################################################
-#reads csv data
-
-def read_csv(filename, mode):
-    """Reads smet data and returns the data in required dataformat (pd df)
-
-    Parameters
-    ----
-    filename : SMET file to read
-    mode :     "d" for daily and "h" for hourly input
-
-    Returns
-    ----
-    data :     data as pd df
-    """
-
-    if mode == "d":
-
-        daily_data_raw = pd.read_csv(
-            filename,
-            index_col='datum',
-            parse_dates=True,
-            na_values=[9999, 32700],
-            )
-
-        del daily_data_raw.index.name
-
-        daily_data = pd.DataFrame(
-            index=daily_data_raw.index,
-            columns=melodist.columns_daily,
-            data=dict(
-                tmin=daily_data_raw.tmin / 10,
-                tmax=daily_data_raw.tmax / 10,
-                tmean=daily_data_raw.t / 10,
-                precip=daily_data_raw.nied.replace(-1, 0) / 10,
-                glob=daily_data_raw.strahl * 10000 / 3600 / 24., # radiation is in J/cm2, convert to mean hourly radiation in W/m2
-                hum=daily_data_raw.rel,
-                wind=daily_data_raw.vv.replace(999, np.nan) / 10,
-                ),
-            )
-
-        daily_data[['tmin', 'tmax', 'tmean']] += 273.15 # convert temperatures to K
-
-        return daily_data
-
-
-    #hourly mode
-    if mode == "h":
-        hourly_data_obs = pd.read_csv(
-            filename,
-            parse_dates=True,
-            index_col='date',
-        )
-
-        del hourly_data_obs.index.name
-
-        return hourly_data_obs
-
-
-################################################################################
-#writes smet-file
-#time can hard-coded overwrite the times to day values "T00:00"
 
 def write_smet(filename, data, metadata, nodata_value=-999, mode='h', check_nan=True):
     """writes smet files
-    
+
     Parameters
     ----
     filename :    filename/loction of output
     data :        data to write as pandas df
     metadata:     header to write input as dict
     nodata_value: Nodata Value to write/use
-    mode:         defines if to write daily ("d") or continuos data (default 'h') 
+    mode:         defines if to write daily ("d") or continuos data (default 'h')
     check_nan:    will check if only nans in data and if true will not write this colums (default True)
     """
-    
+
     # dictionary
-    #based on smet spec V.1.1 and selfdefined
-    #daily data
+    # based on smet spec V.1.1 and selfdefined
+    # daily data
     dict_d=   {'tmean':'TA',
                'tmin':'TMAX',   #no spec
                'tmax':'TMIN',   #no spec
@@ -395,4 +319,79 @@ def write_smet(filename, data, metadata, nodata_value=-999, mode='h', check_nan=
 
         f.write(data_str)
 
-###############################################################################
+
+def read_single_knmi_file(filename):
+    """reads a single file of KNMI's meteorological time series
+
+    data availability: www.knmi.nl/nederland-nu/klimatologie/uurgegevens
+
+    Args:
+        filename: the file to be opened
+
+    Returns:
+        pandas data frame including time series
+    """
+    hourly_data_obs_raw = pd.read_csv(
+        filename,
+        parse_dates=[['YYYYMMDD', 'HH']],
+        date_parser=lambda yyyymmdd, hh: pd.datetime(int(str(yyyymmdd)[0:4]),
+                                                     int(str(yyyymmdd)[4:6]),
+                                                     int(str(yyyymmdd)[6:8]),
+                                                     int(hh) - 1),
+        skiprows=31,
+        skipinitialspace=True,
+        na_values='',
+        keep_date_col=True,
+    )
+
+    hourly_data_obs_raw.index = hourly_data_obs_raw['YYYYMMDD_HH']
+    hourly_data_obs_raw.index = hourly_data_obs_raw.index + pd.Timedelta(hours=1)
+
+    columns_hourly = ['temp', 'precip', 'glob', 'hum', 'wind', 'ssd']
+
+    hourly_data_obs = pd.DataFrame(
+        index=hourly_data_obs_raw.index,
+        columns=columns_hourly,
+        data=dict(
+            temp=hourly_data_obs_raw['T'] / 10 + 273.15,
+            precip=hourly_data_obs_raw['RH'] / 10,
+            glob=hourly_data_obs_raw['Q'] * 10000 / 3600.,
+            hum=hourly_data_obs_raw['U'],
+            wind=hourly_data_obs_raw['FH'] / 10,
+            ssd=hourly_data_obs_raw['SQ'] * 6,
+        ),
+    )
+    # remove negative values
+    negative_values = hourly_data_obs['precip'] < 0.0
+    hourly_data_obs.loc[negative_values, 'precip'] = 0.0
+    return hourly_data_obs
+
+
+def read_knmi_dataset(directory):
+    """Reads files from a directory and merges the time series
+
+    Please note: For each station, a separate directory must be provided!
+    data availability: www.knmi.nl/nederland-nu/klimatologie/uurgegevens
+
+    Args:
+        directory: directory including the files
+
+    Returns:
+        pandas data frame including time series
+    """
+    filemask = '%s*.txt' % directory
+    filelist = glob.glob(filemask)
+
+    columns_hourly = ['temp', 'precip', 'glob', 'hum', 'wind', 'ssd']
+    ts = pd.DataFrame(columns=columns_hourly)
+
+    first_call = True
+    for file_i in filelist:
+        print(file_i)
+        current = read_single_knmi_file(file_i)
+        if(first_call):
+            ts = current
+            first_call = False
+        else:
+            ts = pd.concat([ts, current])
+    return ts
