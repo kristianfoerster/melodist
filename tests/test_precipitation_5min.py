04fcc87a09428de18b6221c9d73ff93e83bfc2a3
Florian Hanzer <florian.hanzer@uibk.ac.at>
2018-11-20 14:42:46 +0100

    Add test for 5-minute precipitation disaggregation

---
+++ tests/test_precipitation_5min.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import melodist
+import unittest
+
+
+class Test5MinutePrecipitation(unittest.TestCase):
+    def test_cascade(self):
+        data = pd.read_csv(
+            'examples/testdata_precip5min.csv.gz',
+            names=['time', 'precip'],
+            parse_dates=True,
+            index_col=0,
+        )
+        precip_daily = data.precip.resample('D').sum()
+
+        cascopt = melodist.build_casc(data, hourly=False, level=9, percentile=90)
+        precip_disagg = melodist.disagg_prec_cascade(precip_daily, cascopt[0], hourly=False, level=9)
+
+        assert np.allclose(precip_daily, precip_disagg.resample('D').sum(), atol=1e-3, equal_nan=True)
